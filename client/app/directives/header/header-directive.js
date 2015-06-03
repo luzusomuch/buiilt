f6dd31482b0c67c08107dc98cf2b652bb351d136
Tuong Tran <tuong.tran@outlook.com>
2015-06-03 20:31:50 +0700

    [-] fix header directive to query projects once user is logged in

--- client/app/directives/header/header-directive.js
+++ client/app/directives/header/header-directive.js
@@ -1,55 +1,72 @@
 angular.module('buiiltApp')
-  .directive('builtHeader', function () {
-    return {
-      restrict: 'E',
-      templateUrl: 'app/directives/header/header.html',
-      controller: function ($scope, authService, projectService, $rootScope, $cookieStore, packageService) {
-        $scope.isLoggedIn=authService.isLoggedIn;
-        $scope.user = authService.getCurrentUser();
+.directive('builtHeader', function($rootScope) {
+  return {
+    restrict: 'E',
+    templateUrl: 'app/directives/header/header.html',
+    controller: function($scope, authService, projectService) {
 
-        $scope.menuTypes = {
-          homeOwner: [{sref:'dashboard',label:'dashboard'},
-            {sref:'client',label:'builder'},
-            {sref:'project',label:'project'}],
-          contractor: [{sref:'dashboard',label:'dashboard'},
-            {sref:'contractors',label:'contractors'},
-            {sref:'project',label:'project'}],
-          buider: [{sref:'dashboard',label:'dashboard'},
-            {sref:'client',label:'client'},
-            {sref:'contractors',label:'contractors'},
-            {sref:'materials',label:'materials'},
-            {sref:'staff',label:'staff'},
-            {sref:'project',label:'project'}],
-          supplier: [{sref:'dashboard',label:'dashboard'},
-            {sref:'contractors',label:'contractors'},
-            {sref:'project',label:'project'}]
-        };
-        
-        projectService.getProjectsByUser({'id': $scope.user._id}, function(projects) {
-          $scope.projects = projects;
-          angular.forEach(projects, function(project) {
-            if ($scope.user._id == project.user) {
-              $scope.tabs = $scope.menuTypes['homeOwner'];
-            }
-          });
-        });
-        projectService.getProjectsByBuilder({'id': $scope.user._id}, function(projects){
-          $scope.projects = projects;
-          angular.forEach(projects, function(project) {
-            if ($scope.user._id == project.builder) {
-              $scope.tabs = $scope.menuTypes['buider'];
-            }
-          });
-        });
+      function queryProjects(){
+        authService.isLoggedInAsync(function(isLoggedIn){
+          if(isLoggedIn){
+            $scope.isLoggedIn = true;
+            $scope.user = authService.getCurrentUser();
 
-        // $scope.user = authService.getCurrentUser();
-        
-       // $scope.loadMenu = function () {
-       //   if ($scope.user._id) {
-       //     $scope.tabs=$scope.menuTypes[$scope.user.type];
-       //   }
-       // };
-       // $scope.loadMenu();
-      }
-    };
-  });
+            projectService.getProjectsByUser({id: $scope.user._id}, function(projects) {
+              $scope.projects = projects;
+              angular.forEach(projects, function(project) {
+                if ($scope.user._id === project.user) {
+                  $scope.tabs = $scope.menuTypes['homeOwner'];
+                }
+              });
+            });
+            projectService.getProjectsByBuilder({'id': $scope.user._id}, function(projects) {
+              $scope.projects = projects;
+              angular.forEach(projects, function(project) {
+                if ($scope.user._id === project.builder) {
+                  $scope.tabs = $scope.menuTypes['buider'];
+                }
+              });
+            });
+          }else{
+            $scope.isLoggedIn = false;
+          }
+        });
+      };
+
+      //first load
+      queryProjects();
+
+      //check menu when state changes
+      $rootScope.$on('$stateChangeSuccess', function (event, next) {
+        queryProjects();
+      });
+
+      $scope.menuTypes = {
+        homeOwner: [{sref: 'dashboard', label: 'dashboard'},
+          {sref: 'client', label: 'builder'},
+          {sref: 'project', label: 'project'}],
+        contractor: [{sref: 'dashboard', label: 'dashboard'},
+          {sref: 'contractors', label: 'contractors'},
+          {sref: 'project', label: 'project'}],
+        buider: [{sref: 'dashboard', label: 'dashboard'},
+          {sref: 'client', label: 'client'},
+          {sref: 'contractors', label: 'contractors'},
+          {sref: 'materials', label: 'materials'},
+          {sref: 'staff', label: 'staff'},
+          {sref: 'project', label: 'project'}],
+        supplier: [{sref: 'dashboard', label: 'dashboard'},
+          {sref: 'contractors', label: 'contractors'},
+          {sref: 'project', label: 'project'}]
+      };
+
+      // $scope.user = authService.getCurrentUser();
+
+      // $scope.loadMenu = function () {
+      //   if ($scope.user._id) {
+      //     $scope.tabs=$scope.menuTypes[$scope.user.type];
+      //   }
+      // };
+      // $scope.loadMenu();
+    }
+  };
+});
