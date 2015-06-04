8c5c76a466cde1f9b57c4664a9d2e94c4ba3b4ee
Vinh Nguyen <vinhnq@songphi.com>
2015-06-04 10:54:31 +0700

    [+] add contractor package

--- client/app/modules/contractors/contractors-controller.js
+++ client/app/modules/contractors/contractors-controller.js
@@ -1,14 +1,18 @@
 angular.module('buiiltApp').controller('ContractorsCtrl', function($scope, $timeout, $q, contractorService, userService, projectService) {
   $scope.contractor = {};
+  $scope.emailsPhone = [];
   $scope.user = userService.get();
 
   projectService.getProjectsByUser({'id': $scope.user._id}, function(projects) {
     $scope.projects = projects;
   });
 
+  $scope.addUser = function() {
+    $scope.emailsPhone.push({email: $scope.newEmail, phoneNumber: $scope.newPhoneNumber});
+  };
+
   $scope.createContractorPackage = function(){
-    // console.log($scope.contractor);
-    contractorService.createContractorPackage($scope.contractor).$promise.then(function(data) {
+    contractorService.createContractorPackage({contractor: $scope.contractor,emailsPhone: $scope.emailsPhone}).$promise.then(function(data) {
 
     });
   };
--- server/api/contractors/contractors.controller.js
+++ server/api/contractors/contractors.controller.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var ContractorPackage = require('./../../models/contractorPackage.model');
+var User = require('./../../models/user.model');
+var _ = require('lodash');
 
 /**
  * Get list of users
@@ -19,16 +21,34 @@ exports.index = function (req, res) {
  * Creates a new contractor package
  */
 exports.createContractorPackage = function (req, res, next) {
+  var to = [];
   var contractorPackage = new ContractorPackage({
     owner: req.user._id,
-    name: req.body.name,
-    description: req.body.description,
-    project: req.body.project
+    name: req.body.contractor.name,
+    description: req.body.contractor.description,
+    project: req.body.contractor.project
   });
-  contractorPackage.save(function(err, saved) {
-    if (err) {return res.send(500, err);}
-    else {
-        return res.json(200, saved);
-    }
+  _.each(req.body.emailsPhone, function(emailPhone){
+    User.findOne({'email': emailPhone.email}, function(err, user) {
+      if (err) {return res.send(500,err);}
+      else {
+        emailPhone._id = user._id
+        to.push(emailPhone);
+        contractorPackage.to = to;
+        contractorPackage.save(function(err, saved) {
+          if (err) {return res.send(500,err);}
+          else {
+            return res.json(200,saved);
+          }
+        })
+      }
+    });
   });
+
+  // contractorPackage.save(function(err, saved) {
+  //   if (err) {return res.send(500, err);}
+  //   else {
+  //       return res.json(200, saved);
+  //   }
+  // });
 };
