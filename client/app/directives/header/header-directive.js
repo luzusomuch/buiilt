4f08672618cb36529fd648cfbc8dad9171ff9fe0
Nghia qt <nghiaqt2902@gmail.com>
2015-06-26 22:33:15 +0700

    [-] fix something

--- client/app/directives/header/header-directive.js
+++ client/app/directives/header/header-directive.js
@@ -24,94 +24,24 @@ angular.module('buiiltApp')
                   .then(function(res) {
                     $scope.currentTeam = res;
                     $scope.projects = $scope.currentTeam.project;
-                    contractorService.getProjectForContractor({'id': $scope.user._id}, function(result) {
-                     $scope.projectsContractor = result;
-                    });
-                    materialPackageService.getProjectForSupplier({'id': $scope.user._id}, function(result) {
-                     $scope.projectsSupplier = result;
-                    });
-                    
                     if ($stateParams.id) {
-                      projectService.get({'id': $stateParams.id}).$promise.then(function(project) {
-                        if (!project && project == null) {
-                          var userId = $scope.user._id;
-                          $scope.tabs = [{sref: 'team.manager', label: 'team manager'},
-                            {sref: 'user.form({id: userId})', label: 'edit profile'},
-                            {sref: 'notification.view({id: userId})', label: 'notification'}];
-                        }
-                        else {
-                          if ($scope.currentTeam.type === 'homeOwner') {
-                            $scope.tabs = $scope.menuTypes['homeOwner'];
-                          }
-                          else if($scope.currentTeam.type === 'builder') {
-                            $scope.tabs = $scope.menuTypes['builder'];
-                          }
-                          else if($scope.currentTeam.type === 'contractor') {
-                            $scope.tabs = $scope.menuTypes['contractor'];
-                          }
-                          else if($scope.currentTeam.type === 'supplier') {
-                            $scope.tabs = $scope.menuTypes['supplier'];
-                          }
-                        }
-                      });
-                    }
-                    else {
-                      var userId = $scope.user._id;
-                      $scope.tabs = [{sref: 'team.manager', label: 'team manager'},
-                        {sref: 'user.form', label: 'edit profile'},
-                        {sref: 'notification.view', label: 'notification'}];
+                      if ($scope.currentTeam.type === 'homeOwner') {
+                        $scope.tabs = $scope.menuTypes['homeOwner'];
+                      }
+                      else if ($scope.currentTeam.type === 'builder') {
+                        $scope.tabs = $scope.menuTypes['builder'];
+                      }
+                      else if ($scope.currentTeam.type === 'contractor') {
+                        $scope.tabs = $scope.menuTypes['contractor'];
+                      }
+                      else if ($scope.currentTeam.type === 'supplier') {
+                        $scope.tabs = $scope.menuTypes['supplier'];
+                      }
+                    } else {
+                      $scope.tabs = $scope.menuTypes['other'];
                     }
                   });
               });
-
-
-            teamService.getHomeOwnerTeam().$promise.then(function(data){
-             $scope.homeOwnerTeams = data;
-             var homeOwnerTeamMember = [];
-             angular.forEach($scope.homeOwnerTeams, function(homeOwnerTeam) {
-               _.each(homeOwnerTeam.leader, function(leader) {
-                 homeOwnerTeamMember.push({_id: leader._id, email: leader.email});
-               });
-               _.each(homeOwnerTeam.member, function(member) {
-                 if (member._id && member.status === 'Active') {
-                   homeOwnerTeamMember.push({_id: member._id._id, email: member._id.email});
-                 }
-               })
-             });
-             $scope.homeOwnerTeamMember = homeOwnerTeamMember;
-             // console.log($scope.homeOwnerTeamMember);
-            });
-            
-            teamService.getHomeBuilderTeam().$promise.then(function(data) {
-             $scope.homeBuilderTeams = data;
-             var homeBuilderTeamMember = [];
-             angular.forEach($scope.homeBuilderTeams, function(homeBuilderTeam) {
-               _.each(homeBuilderTeam.leader, function(leader) {
-                 homeBuilderTeamMember.push({_id: leader._id, email: leader.email});
-               });
-               _.each(homeBuilderTeam.member, function(member){
-                 if (member._id) {
-                   homeBuilderTeamMember.push({_id: member._id._id, email: member._id.email});
-                 }
-               })
-             });
-             $scope.homeBuilderTeamMember = homeBuilderTeamMember;
-            });
-            //
-            //// console.log($scope.homeOwnerTeams.member);
-             // $scope.homeOwnerTeams.member  = filterFilter($scope.homeOwnerTeams.member, {status : 'Active'});
-            //
-            // projectService.getProjectsByUser({'id': $scope.user._id}, function(projects) {
-            //  $scope.projectsOwner = projects;
-            // });
-            // projectService.getProjectsByBuilder({'id': $scope.user._id}, function(projects) {
-            //  $scope.projectsBuilder = projects;
-            // });
-            
-
-
-
-
 
           } else {
             $scope.isLoggedIn = false;
@@ -144,15 +74,25 @@ angular.module('buiiltApp')
           {sref: 'projects.view({id :  currentProject._id})', label: 'project'}],
         supplier: [{sref: 'dashboard({id :  currentProject._id})', label: 'dashboard'},
           {sref: 'materials({id :  currentProject._id})', label: 'materials'},
-          {sref: 'projects.view({id :  currentProject._id})', label: 'project'}]
+          {sref: 'projects.view({id :  currentProject._id})', label: 'project'}],
+        other : [{sref: 'team.manager', label: 'team manager'},
+          {sref: 'user.form', label: 'edit profile'},
+          {sref: 'notification.view', label: 'notification'}]
       };
 
       $scope.create = function(form) {
         $scope.submitted = true;
         if (form.$valid) {
             projectService.create($scope.project).$promise.then(function(data) {
-              alert('Create project successfully!');
               $scope.projects.push(data);
+              $scope.project = {
+                package : {
+                  location: {},
+                  to : ''
+                }
+              };
+              $scope.submitted = false;
+              $('#createProject').closeModal();
             }, function(res) {
               $scope.errors = res.data;
             });
