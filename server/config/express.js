34fc95de5f5d5523f1b6ec5ac01be0b97cab7398
Vinh Nguyen <vinhnq@songphi.com>
2015-07-20 14:43:32 +0700

    [+] add allow access in express.js

--- server/config/express.js
+++ server/config/express.js
@@ -53,6 +53,25 @@ module.exports = function(app) {
   app.use(cookieParser());
   app.use(passport.initialize());
 
+  // Add headers
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+  });
+
   //request loger
   app.use(logger.requestsLogger);
   // error handlers
