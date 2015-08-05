37c2964de025517d5deff171274ef46aecf27055
Vinh Nguyen <vinhnq@songphi.com>
2015-08-05 13:48:42 +0700

    [+]sortable for project list [+] view detail for document

--- client/app/modules/backend/document-backend/main.js
+++ client/app/modules/backend/document-backend/main.js
@@ -2,6 +2,12 @@ angular.module('buiiltApp').config(function($stateProvider) {
   $stateProvider
   .state('documentBackend', {
     url: '/backend/document',
+    hasCurrentProject : true,
+    authenticate : true,
+    template: '<ui-view/>'
+  })
+  .state('documentBackend.list', {
+    url: '/list',
     templateUrl: '/app/modules/backend/document-backend/index.html',
     controller: 'DocumentBackendCtrl',
     authenticate: true,
@@ -9,6 +15,19 @@ angular.module('buiiltApp').config(function($stateProvider) {
         documents: function(fileService) {
             return fileService.getAll().$promise;
         }
+    }
+  })
+  .state('documentBackend.detail', {
+    url: '/:documentId',
+    templateUrl: '/app/modules/backend/document-backend/detail/view.html',
+    controller: 'DocumentDetailBackendCtrl',
+    authenticate: true,
+    resolve: {
+      document: function(fileService, $stateParams) {
+        console.log('asdasdasd');
+        console.log($stateParams.documentId);
+        return fileService.get({id: $stateParams.documentId}).$promise;
+      }
     }
   })
 });
--- client/app/modules/backend/project-backend/project-backend-controller.js
+++ client/app/modules/backend/project-backend/project-backend-controller.js
@@ -1,13 +1,19 @@
-angular.module('buiiltApp').controller('ProjectBackendCtrl', function($scope, projects, projectService,ngTableParams) {
+angular.module('buiiltApp').controller('ProjectBackendCtrl', function($scope, projects, projectService,ngTableParams,$filter) {
     var data = projects;
 
     $scope.tableParams = new ngTableParams({
         page: 1,            // show first page
-        count: 15           // count per page
+        count: 15,           // count per page
+        sorting: {
+            name: 'asc'     // initial sorting
+        }
     }, {
         total: data.length, // length of data
         getData: function ($defer, params) {
-            $defer.resolve(data.slice((params.page() - 1) * params.count(), params.page() * params.count()));
+            var orderedData = params.sorting() ?
+                    $filter('orderBy')(data, params.orderBy()) :
+                    data;
+            $defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
         }
     });
 
