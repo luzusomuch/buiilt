e442dba3f75eef3d5d14c36adc8edefe023a6d90
Vinh Nguyen <vinhnq@songphi.com>
2015-08-24 09:48:59 +0700

    test

--- server/api/upload/upload.controller.js
+++ server/api/upload/upload.controller.js
@@ -491,9 +491,10 @@ exports.uploadInPackge = function(req, res){
                                         });
                                     }
                                     else if (saved.mimeType == 'application/pdf') {
+                                        console.log('saved._id');
                                         // console.log("'"+'C:/Program Files (x86)/gs/gs9.16/bin/gswin32c.exe -dNOPAUSE -sDEVICE=jpeg -r144 -sOutputFile= '+config.media +saved._id + "-" +saved.title+".jpg "+ config.root + "/" + saved.path+"'");
                                         // exec("'"+'C:/Program Files (x86)/gs/gs9.16/bin/gswin32c.exe -dNOPAUSE -sDEVICE=jpeg -r144 -sOutputFile= '+config.media +saved._id + "-" +saved.title+".jpg "+ config.root + "/" + saved.path+"'", function(err,stdout,stderr){
-                                        exec("'"+'gs -dNOPAUSE -sDEVICE=jpeg -dFirstPage=1 -dLastPage=1 -r144 -sOutputFile='+config.media +saved._id + "-" +saved.title+".jpg "+ config.root +"/" + saved.path+"'", function(err,stdout,stderr){
+                                        exec('gs -dNOPAUSE -sDEVICE=jpeg -dFirstPage=1 -dLastPage=1 -r144 -sOutputFile=config.media +saved._id + "-" +saved.title+".jpg "+ config.root +"/" + saved.path', function(err,stdout,stderr){
                                             if (err) {console.log('err' + err.code);return cb(err);}
                                             else {
                                                 console.log(stdout);
