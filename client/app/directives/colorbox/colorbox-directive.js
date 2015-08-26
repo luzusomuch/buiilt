a5f0264c1b9fddda0af56ce4cae4352b75d8e8a1
Vinh Nguyen <vinhnq@songphi.com>
2015-08-26 08:45:33 +0700

    [-] fix colorbox image

--- client/app/directives/colorbox/colorbox-directive.js
+++ client/app/directives/colorbox/colorbox-directive.js
@@ -9,8 +9,9 @@ angular.module('buiiltApp').directive('colorbox', function($compile, $rootScope)
                     onComplete: function(){
                         $rootScope.$apply(function(){
                             var content = $('#cboxLoadedContent');
+                            $("div#cboxOverlay").css('background','#ccc');
                             $("div#cboxLoadedContent").css('height',scope.maximunHeight+'px');
-                            $("img.cboxPhoto").css({'height':'100%','withd':'auto'});
+                            $("img.cboxPhoto").css({'height':'100%','width':'auto'});
                             $compile(content)($rootScope);      
                         });
                     }
