626ca799b3f99b410123916f5b006a94807c728d
Vinh Nguyen <vinhnq@songphi.com>
2015-09-05 15:52:51 +0700

    [+] add multi view for colorbox in documentation

--- client/app/directives/colorbox/colorbox-directive.js
+++ client/app/directives/colorbox/colorbox-directive.js
@@ -1,22 +1,32 @@
 'use strict';
 angular.module('buiiltApp').directive('colorbox', function($compile, $rootScope){
     return {
+        restrict: 'A',
         link: function(scope, element, attrs){
-            element.click('bind', function(){
-                scope.maximunHeight = $rootScope.maximunHeight - 30;
-                $.colorbox({
-                    href: attrs.colorbox,
-                    onComplete: function(){
-                        $rootScope.$apply(function(){
-                            var content = $('#cboxLoadedContent');
-                            $("div#cboxOverlay").css('background','#ccc');
-                            $("div#cboxLoadedContent").css('height',scope.maximunHeight+'px');
-                            $("img.cboxPhoto").css({'height':'100%','width':'auto'});
-                            $compile(content)($rootScope);      
-                        });
-                    }
+            element.click('bind', function(e){
+                e.preventDefault();
+                var gallery = $(element).children().children();
+                var elementClass = "a."+$(gallery[0]).attr('class');
+                // scope.maximunHeight = $rootScope.maximunHeight - 30;
+                $(elementClass).colorbox({
+                    href: attrs.href,
+                    rel: function(){return $(this).data('rel');},
+                    open: true
+                    // rel: attrs.rel,
+                    // onComplete: function(){
+                    //     $rootScope.$apply(function(){
+                    //         var content = $('#cboxLoadedContent');
+                    //         $("div#cboxOverlay").css('background','#ccc');
+                    //         $("div#cboxLoadedContent").css('height',scope.maximunHeight+'px');
+                    //         $("img.cboxPhoto").css({'height':'100%','width':'auto'});
+                    //         $compile(content)($rootScope);      
+                    //     });
+                    // }
                 });
             });
+            // scope.maximunHeight = $rootScope.maximunHeight - 30;
+
+            // $(element).colorbox(attrs.colorbox);
         }
     };
 });
