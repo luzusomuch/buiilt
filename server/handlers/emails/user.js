3f45990313e32d6015892ecb07e06c2e6e23e866
Vinh Nguyen <vinhnq@songphi.com>
2015-09-10 11:08:14 +0700

    [-]fix conflict

--- server/handlers/emails/user.js
+++ server/handlers/emails/user.js
@@ -34,11 +34,7 @@ EventBus.onSeries('User.ChangeEmail', function(user, next) {
     user: user.toJSON(),
     emailChange : user.emailChange,
     confirmation: config.baseUrl + 'auth/confirm-email-change/' + user.changeEmailToken,
-<<<<<<< HEAD
-    subject: 'Please confirm your email address with buiilt'
-=======
     subject: 'Please confirm your password change for buiilt'
->>>>>>> 1caf6089273d2757f87127ead18b68c86d4a5913
   }, function(err){
     return next();
   });
