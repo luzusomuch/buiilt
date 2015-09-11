b65fff3c680695fff423937a895105c9e62eceac
Vinh Nguyen <vinhnq@songphi.com>
2015-09-11 14:18:26 +0700

    [-] fix resize of image with lightbox

--- client/app/directives/colorbox/colorbox-directive.js
+++ client/app/directives/colorbox/colorbox-directive.js
@@ -8,6 +8,7 @@ angular.module('buiiltApp').directive('colorbox', function($compile, $rootScope)
                 var gallery = $(element).children().children();
                 var elementClass = "a."+$(gallery[0]).attr('class');
                 scope.maximunHeight = $rootScope.maximunHeight - 30;
+                var screenWidth = $(window).width();
                 console.log(scope.maximunHeight);
                 $(elementClass).colorbox({
                     href: attrs.href,
@@ -16,11 +17,17 @@ angular.module('buiiltApp').directive('colorbox', function($compile, $rootScope)
                     // rel: attrs.rel,
                     onComplete: function(){
                         $rootScope.$apply(function(){
+                            var type = $(elementClass).data('type');
                             var content = $('#cboxLoadedContent');
                             $("div#cboxOverlay").css('background','#ccc');
                             $("div#cboxContent").css({'height':scope.maximunHeight+'px','width':'auto'});
                             $("div#cboxLoadedContent").css({'height':scope.maximunHeight+'px','width':'auto'});
-                            $("img.cboxPhoto").css({'height':'auto','width':'100%'});
+                            if (type == 'jgp') {
+                                $("img.cboxPhoto").css({'height':'auto','width':'100%'});
+                            }
+                            else if (type == 'pdf') {
+                                $("img.cboxPhoto").css({'height':'auto','width':screenWidth+'px'});   
+                            }
                             $compile(content)($rootScope);      
                         });
                     }
