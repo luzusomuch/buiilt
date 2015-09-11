4a1e15dbc4eb3962c498c8667e3454628b4f77c1
Vinh Nguyen <vinhnq@songphi.com>
2015-09-11 14:00:21 +0700

    [-] fix size of color box

--- client/app/directives/colorbox/colorbox-directive.js
+++ client/app/directives/colorbox/colorbox-directive.js
@@ -7,21 +7,23 @@ angular.module('buiiltApp').directive('colorbox', function($compile, $rootScope)
                 e.preventDefault();
                 var gallery = $(element).children().children();
                 var elementClass = "a."+$(gallery[0]).attr('class');
-                // scope.maximunHeight = $rootScope.maximunHeight - 30;
+                scope.maximunHeight = $rootScope.maximunHeight - 30;
+                console.log(scope.maximunHeight);
                 $(elementClass).colorbox({
                     href: attrs.href,
                     rel: function(){return $(this).data('rel');},
-                    open: true
+                    open: true,
                     // rel: attrs.rel,
-                    // onComplete: function(){
-                    //     $rootScope.$apply(function(){
-                    //         var content = $('#cboxLoadedContent');
-                    //         $("div#cboxOverlay").css('background','#ccc');
-                    //         $("div#cboxLoadedContent").css('height',scope.maximunHeight+'px');
-                    //         $("img.cboxPhoto").css({'height':'100%','width':'auto'});
-                    //         $compile(content)($rootScope);      
-                    //     });
-                    // }
+                    onComplete: function(){
+                        $rootScope.$apply(function(){
+                            var content = $('#cboxLoadedContent');
+                            $("div#cboxOverlay").css('background','#ccc');
+                            $("div#cboxContent").css({'height':scope.maximunHeight+'px','width':'auto'});
+                            $("div#cboxLoadedContent").css({'height':scope.maximunHeight+'px','width':'auto'});
+                            $("img.cboxPhoto").css({'height':'auto','width':'100%'});
+                            $compile(content)($rootScope);      
+                        });
+                    }
                 });
             });
             // scope.maximunHeight = $rootScope.maximunHeight - 30;
