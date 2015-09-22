79eff077313fcae11c2190015520cfce6e72361c
Vinh Nguyen <vinhnq@songphi.com>
2015-09-22 17:38:33 +0700

    [-] fix showing bug in create new design package

--- client/app/modules/design/index/design-controller.js
+++ client/app/modules/design/index/design-controller.js
@@ -83,49 +83,53 @@ angular.module('buiiltApp')
         // console.log(builderPackage);
         $scope.available = builderPackage.owner.leader;
         var availableEachTeam = [];
-        if (builderPackage.owner.member.length > 0) {
-            _.each(builderPackage.owner.member, function(member){
-                if (member.status == 'Active') {
-                    $scope.available.push(member._id);
-                }
-            });
-        }
+        // if (builderPackage.owner.member.length > 0) {
+        //     _.each(builderPackage.owner.member, function(member){
+        //         if (member.status == 'Active') {
+        //             $scope.available.push(member._id);
+        //         }
+        //     });
+        // }
         if (builderPackage.to) {
             _.each(builderPackage.to.team.leader, function(leader){
                 availableEachTeam.push(leader);
             });
-            if (builderPackage.to.team.member.length > 0) {
-                _.each(builderPackage.to.team.member, function(member){
-                    if (member._id || member.status == 'Active') {
-                        availableEachTeam.push(member._id);
-                    }
-                });
-            }
+            // if (builderPackage.to.team.member.length > 0) {
+            //     _.each(builderPackage.to.team.member, function(member){
+            //         if (member._id || member.status == 'Active') {
+            //             availableEachTeam.push(member._id);
+            //         }
+            //     });
+            // }
             $scope.available = _.union($scope.available, availableEachTeam);
         }
         if (builderPackage.winner) {
             _.each(builderPackage.winner.leader, function(leader) {
                 availableEachTeam.push(leader);
             });
-            if (builderPackage.winner.member.length > 0) {
-                _.each(builderPackage.winner.member, function(member) {
-                    availableEachTeam.push(member)
-                });
-            }
+            // if (builderPackage.winner.member.length > 0) {
+            //     _.each(builderPackage.winner.member, function(member) {
+            //         availableEachTeam.push(member)
+            //     });
+            // }
             $scope.available = _.union($scope.available, availableEachTeam);
         }
-        if (builderPackage.architect.team._id) {
+        if (builderPackage.architect.team._id != $scope.currentTeam._id) {
             _.each(builderPackage.architect.team.leader, function(leader) {
                 availableEachTeam.push(leader);
             });
-            if (builderPackage.architect.team.member.length > 0) {
-                _.each(builderPackage.architect.team.member, function(member) {
-                    availableEachTeam.push(member)
-                });
-            }
+            // if (builderPackage.architect.team.member.length > 0) {
+            //     _.each(builderPackage.architect.team.member, function(member) {
+            //         availableEachTeam.push(member)
+            //     });
+            // }
             $scope.available = _.union($scope.available, availableEachTeam);
         }
-        console.log($scope.available);
+        if ($scope.currentTeam.member.length > 0) {
+            _.each($scope.currentTeam.member, function(member){
+                available.push(member);
+            })
+        }
     };
 
     getAvailableAssign();
