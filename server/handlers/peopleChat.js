daaf29f7491f4a67fcaca7256229d61187ae6490
Vinh Nguyen <vinhnq@songphi.com>
2015-11-11 18:17:10 +0700

    [-] fix bugs when send message

--- server/handlers/peopleChat.js
+++ server/handlers/peopleChat.js
@@ -14,19 +14,23 @@ var mongoose = require('mongoose');
 EventBus.onSeries('PeopleChat.Updated', function(req, next) {
     _.remove(req.members, req.editUser._id);
     var newestMessage = _.last(req.messages);
-    if (newestMessage.mentions.length > 0) {
-        var params = {
-            owners: newestMessage.mentions,
-            fromUser: req.editUser._id,
-            element: req,
-            referenceTo: 'people-chat',
-            type: 'chat'
-        };
-        NotificationHelper.create(params, function() {
-            next();
-        });
-        var data = _.last(req.messages);
-        PushNotificationHelper.getData(req.project,req._id,'People package', data.text, data.mentions, 'people');
+    if (newestMessage.mentions) {
+        if (newestMessage.mentions.length > 0) {
+            var params = {
+                owners: newestMessage.mentions,
+                fromUser: req.editUser._id,
+                element: req,
+                referenceTo: 'people-chat',
+                type: 'chat'
+            };
+            NotificationHelper.create(params, function() {
+                next();
+            });
+            var data = _.last(req.messages);
+            PushNotificationHelper.getData(req.project,req._id,'People package', data.text, data.mentions, 'people');
+        } else {
+            return next();
+        }
     } else {
         return next();
     }
