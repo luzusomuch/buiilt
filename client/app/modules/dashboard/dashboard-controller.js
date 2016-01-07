a5988b9633955af5040df41a951bf01203cfb254
Vinh Nguyen <vinhnq@songphi.com>
2016-01-07 10:04:47 +0700

    [+]style of md-chip, multiple filter

--- client/app/modules/dashboard/dashboard-controller.js
+++ client/app/modules/dashboard/dashboard-controller.js
@@ -6,7 +6,7 @@ angular.module('buiiltApp').controller('dashboardCtrl', function($rootScope, $sc
 	$scope.search = false;
 	// type can be a task, message, file or document
 	var type = $state.current.name.split(".")[1];
-    
+    $scope.results = [];
 	
 	//Placeholder Set of Filters to use for layout demo
 	$scope.dashboardFilters = [];
@@ -50,12 +50,11 @@ angular.module('buiiltApp').controller('dashboardCtrl', function($rootScope, $sc
     };
 
     function filterByProject(results) {
-    	$scope.results = [];
     	if (type === 'tasks') {
     		_.each($scope.myTasks, function(task) {
     			if (_.last(results)) {
 		        	if (task.project._id.toString() === _.last(results).project._id.toString()) {
-		        		$scope.results.push(task);
+                        getHandleArr(task);
 		        	}
     			}
 	        });
@@ -63,7 +62,7 @@ angular.module('buiiltApp').controller('dashboardCtrl', function($rootScope, $sc
     		_.each($scope.myMessages, function(message) {
     			if (_.last(results)) {
 		        	if (message.project._id.toString() === _.last(results).project._id.toString()) {
-		        		$scope.results.push(message);
+		        		getHandleArr(message);
 		        	}
     			}
 	        });
@@ -71,7 +70,7 @@ angular.module('buiiltApp').controller('dashboardCtrl', function($rootScope, $sc
     		_.each($scope.myFiles, function(file) {
     			if (_.last(results)) {
 		        	if (file.project._id.toString() === _.last(results).project._id.toString()) {
-		        		$scope.results.push(file);
+		        		getHandleArr(file);
 		        	}
     			}
 	        });
@@ -79,7 +78,7 @@ angular.module('buiiltApp').controller('dashboardCtrl', function($rootScope, $sc
     		_.each($scope.myFiles, function(file) {
     			if (_.last(results)) {
 		        	if (file.project._id.toString() === _.last(results).project._id.toString()) {
-		        		$scope.results.push(file);
+		        		getHandleArr(file);
 		        	}
     			}
 	        });
@@ -97,107 +96,131 @@ angular.module('buiiltApp').controller('dashboardCtrl', function($rootScope, $sc
         }
     };
 
+    var lastFilterResults = [];
     $scope.selectedChip = function(chip) {
-        $scope.results = [];
+        chip.select = !chip.select;
         $scope.search = true;
         var user = $rootScope.currentUser;
-        if (chip.place === "task") {
-            switch (chip.value) {
-                case "assignedToMe":
-                    _.each($scope.myTasks, function(task) {
-                        if (_.findIndex(task.assignees, function(assignee) {
-                            return assignee._id == user._id;
-                        }) !== -1) {
-                            $scope.results.push(task);
-                        }
-                    });
-                break;
-
-                case "assignedByMe":
-                    _.each($scope.myTasks, function(task) {
-                        if (task.user && task.user._id == user._id) {
-                            $scope.results.push(task);
-                        }
-                    });
-                break;
-
-                case "dueToDay":
-                    _.each($scope.myTasks, function(task) {
-                        if (moment(moment(task.dateEnd).format("YYYY-MM-DD")).isSame(moment(new Date()).format("YYYY-MM-DD"))) {
-                            $scope.results.push(task);
-                        }
-                    });
-                break;
-
-                case "dueTomorrow":
-                    var tomorrow = moment(new Date()).add(1, "days").format("YYYY-MM-DD");
-                    _.each($scope.myTasks, function(task) {
-                        var dateEnd = moment(task.dateEnd).format("YYYY-MM-DD");
-                        if (moment(dateEnd).isSame(tomorrow)) {
-                            $scope.results.push(task);
-                        }
-                    });
-                break;
-
-                case "dueThisWeek":
-                    _.each($scope.myTasks, function(task) {
-                        var currentWeek = getWeek(new Date());
-                        _.each(currentWeek, function(day) {
-                            if (moment(moment(new Date(day)).format("YYYY-MM-DD")).isSame(moment(new Date(task.dateEnd)).format("YYYY-MM-DD"))) {
-                                $scope.results.push(task);
+        if (chip.select) {
+            if (chip.place === "task") {
+                switch (chip.value) {
+                    case "assignedToMe":
+                        _.each($scope.myTasks, function(task) {
+                            if (_.findIndex(task.assignees, function(assignee) {
+                                return assignee._id == user._id;
+                            }) !== -1) {
+                                lastFilterResults.push(task);
+                                getHandleArr(task);
                             }
                         });
-                    });
-                break;
+                    break;
 
-                default:
-                break;
-            }
-        } else if (chip.place === "message") {
-            switch (chip.value) {
-                case "createdByMe":
-                    _.each($scope.myMessages, function(item) {
-                        if (item.owner == user._id) {
-                            $scope.results.push(item);
-                        }
-                    });
-                break;
+                    case "assignedByMe":
+                        _.each($scope.myTasks, function(task) {
+                            if (task.user && task.user._id == user._id) {
+                                lastFilterResults.push(task);
+                                getHandleArr(task);
+                            }
+                        });
+                    break;
 
-                case "mentionsMe":
-                    _.each($scope.myMessages, function(item) {
-                        if (item.messages.length > 0) {
-                            _.each(item.messages, function(message) {
-                                if (message.mentions && message.mentions.length > 0) {
-                                    if (_.indexOf(message.mentions, user._id) !== -1) {
-                                        $scope.results.push(item);
-                                        return false;
-                                    }
+                    case "dueToDay":
+                        _.each($scope.myTasks, function(task) {
+                            if (moment(moment(task.dateEnd).format("YYYY-MM-DD")).isSame(moment(new Date()).format("YYYY-MM-DD"))) {
+                                lastFilterResults.push(task);
+                                getHandleArr(task);
+                            }
+                        });
+                    break;
+
+                    case "dueTomorrow":
+                        var tomorrow = moment(new Date()).add(1, "days").format("YYYY-MM-DD");
+                        _.each($scope.myTasks, function(task) {
+                            var dateEnd = moment(task.dateEnd).format("YYYY-MM-DD");
+                            if (moment(dateEnd).isSame(tomorrow)) {
+                                lastFilterResults.push(task);
+                                getHandleArr(task);
+                            }
+                        });
+                    break;
+
+                    case "dueThisWeek":
+                        _.each($scope.myTasks, function(task) {
+                            var currentWeek = getWeek(new Date());
+                            _.each(currentWeek, function(day) {
+                                if (moment(moment(new Date(day)).format("YYYY-MM-DD")).isSame(moment(new Date(task.dateEnd)).format("YYYY-MM-DD"))) {
+                                    lastFilterResults.push(task);
+                                    getHandleArr(task);
                                 }
                             });
-                        }
-                    });
-                break;
+                        });
+                    break;
 
-                default:
-                break;
+                    default:
+                    break;
+                }
+            } else if (chip.place === "message") {
+                switch (chip.value) {
+                    case "createdByMe":
+                        _.each($scope.myMessages, function(item) {
+                            if (item.owner == user._id) {
+                                lastFilterResults.push(item);
+                                getHandleArr(item);
+                            }
+                        });
+                    break;
+
+                    case "mentionsMe":
+                        _.each($scope.myMessages, function(item) {
+                            if (item.messages.length > 0) {
+                                _.each(item.messages, function(message) {
+                                    if (message.mentions && message.mentions.length > 0) {
+                                        if (_.indexOf(message.mentions, user._id) !== -1) {
+                                            lastFilterResults.push(item);
+                                            getHandleArr(item);
+                                            return false;
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    break;
+
+                    default:
+                    break;
+                }
+            }
+        } else {
+            if (lastFilterResults.length > 0 && $scope.results.length > 0) {
+                _.each(lastFilterResults, function(index, result) {
+                    $scope.results = _.remove($scope.results, function(item) {
+                        return item._id == result._Id;
+                    });
+                    lastFilterResults.splice(index, 1);
+                    if ($scope.results.length == 0) {
+                        $scope.search = false;
+                    }
+                });
+            } else {
+                $scope.search = false;
             }
         }
     };
 
     $scope.messageChips = [
-        {place: "message", value: "createdByMe", text: "CREATED BY ME"},
-        {place: "message", value: "mentionsMe", text: "MENTIONS ME"}
+        {place: "message", value: "createdByMe", text: "CREATED BY ME", select: false},
+        {place: "message", value: "mentionsMe", text: "MENTIONS ME", select: false}
     ];
     $scope.taskChips = [
-        {place: "task", value: "assignedToMe", text: "Assigned to Me"},
-        {place: "task", value: "assignedByMe", text: "Assigned by Me"},
-        {place: "task", value: "dueToDay", text: "Due Today"},
-        {place: "task", value: "dueTomorrow", text: "Due Tomorrow"},
-        {place: "task", value: "dueThisWeek", text: "Due This Week"}
+        {place: "task", value: "assignedToMe", text: "Assigned to Me", select: false},
+        {place: "task", value: "assignedByMe", text: "Assigned by Me", select: false},
+        {place: "task", value: "dueToDay", text: "Due Today", select: false},
+        {place: "task", value: "dueTomorrow", text: "Due Tomorrow", select: false},
+        {place: "task", value: "dueThisWeek", text: "Due This Week", select: false}
     ];
     $scope.fileChips = [
-        {place: "file", value: "mine", text: "Mine"},
-        {place: "file", value: "myTeam", text: "My Teams"}
+        {place: "file", value: "mine", text: "Mine", select: false},
+        {place: "file", value: "myTeam", text: "My Teams", select: false}
     ];
 
     function getWeek(fromDate){
@@ -207,5 +230,10 @@ angular.module('buiiltApp').controller('dashboardCtrl', function($rootScope, $sc
             result.push(new Date(sunday));
         }
         return result;
-    }
+    };
+
+    function getHandleArr(item) {
+        $scope.results.push(item);
+        return $scope.results = _.uniq($scope.results, '_id');
+    };
 });
