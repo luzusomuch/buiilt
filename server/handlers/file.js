7848fcd15109b5bbc0c658c9ae374a6c69f4a1ac
Vinh Nguyen <vinhnq@songphi.com>
2016-01-15 16:49:53 +0700

    [+] add email template for file

--- server/handlers/file.js
+++ server/handlers/file.js
@@ -2,59 +2,50 @@
 
 var EventBus = require('./../components/EventBus');
 var User = require('./../models/user.model');
-var File = require('./../models/file.model');
 var Project = require('./../models/project.model');
-var BuilderPackage = require('./../models/builderPackage.model');
 var Team = require('./../models/team.model');
-var Board = require('./../models/board.model');
-var People = require('./../models/people.model');
-var Notification = require('./../models/notification.model');
-var NotificationHelper = require('./../components/helpers/notification');
 var Mailer = require('./../components/Mailer');
+var PackageInvite = require('./../../models/packageInvite.model');
 var _ = require('lodash');
 var async = require('async');
 
 
 EventBus.onSeries('File.Inserted', function(request, next) {
-    if (request.belongToType == 'design') {
-        File.find({project: request.project, belongTo: request.belongTo}, function(err, _files){
-            if (err) {return next();}
-            if (_files.length > 1) {
-                File.findByIdAndRemove(_files[0]._id, function(err){
-                    if (err) {console.log(err);return next();}
-                    return next();
+    async.parallel({
+        project: function(cb) {
+            Project.findById(request.project, cb);
+        }, 
+        editUser: function(cb) {
+            User.findById(request.owner, cb);
+        },
+        team: function(cb) {
+            Team.findOne({$or:[{leader: request.owner}, {member: request.owner}]}, cb);
+        }
+    }, function(err, result) {
+        if (err) {return next();}
+        async.each(request.members, function(member, cb) {
+            if (member.email) {
+                packageInvite.findOne({to: member.email}, function(err, packageInvite) {
+                    if (err || !packageInvite) {cb();}
+                    else {
+                        Mailer.sendMail('upload-file-to-non-user.html', from, member.email, {
+                            team: result.team.toJSON(),
+                            inviter: result.editUser.toJSON(),
+                            invitee: member.email,
+                            project: result.project.toJSON(),
+                            request: request.toJSON(),
+                            link : config.baseUrl + 'signup-invite?packageInviteToken=' + packageInvite._id,
+                            subject: req.editUser.name + ' has uploaded for you a  ' + request.name
+                        },function(err){console.log(err);
+                            return cb();
+                        });
+                    }
                 });
             } else {
-                return next();
+                cb();
             }
+        }, function() {
+            return next();
         });
-    } else if (request.belongToType == "people" || request.belongToType == "board") {
-        var _package = {};
-        if (request.belongToType == "people") {
-            _package = People;
-        } else {
-            _package = Board;
-        }
-        _package.findById(request.belongTo, function(err, _package) {
-            if (err) {console.log(err);return next();}
-            if (!_package) {console.log('no package found!');return next();}
-            _.remove(request.usersRelatedTo, request.user);
-            var referenceTo = "documentIn" + request.belongToType;
-            var params = {
-                owners : request.usersRelatedTo,
-                fromUser : request.user,
-                element : {
-                    file: request,
-                    package: _package
-                },
-                referenceTo : referenceTo,
-                type : 'uploadDocument'
-            };
-            NotificationHelper.create(params, function() {
-                next();
-            });
-        });
-    } else {
-        return next();
-    }
+    });
 });
