9db44a221d358fdb421c52285419c15dcc9d03bb
Vinh Nguyen <vinhnq@songphi.com>
2016-01-27 17:40:56 +0700

    [*] change addendum review window

--- client/app/directives/tenderItems/tenderItems-directive.js
+++ client/app/directives/tenderItems/tenderItems-directive.js
@@ -24,6 +24,11 @@ angular.module('buiiltApp').directive('tenderItems', function(){
                             $mdDialog.cancel();
                         };
 
+                        $scope.goToFileDetail = function() {
+                            $scope.closeModal();
+                            $state.go("project.files.detail", {id: $stateParams.id, fileId: addendum.element._id});
+                        };
+
                         $scope.download = function() {
                             filepicker.exportFile(
                                 {url: addendum.element.link, filename: addendum.element.name},
