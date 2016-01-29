2920d25653573b3ef8642e0d830d4b39ef49ab82
Vinh Nguyen <vinhnq@songphi.com>
2016-01-29 18:24:25 +0700

    [+] send email for new message with non-user

--- server/handlers/emails/thread.js
+++ server/handlers/emails/thread.js
@@ -43,6 +43,56 @@ EventBus.onSeries("Thread.Inserted", function(thread, next) {
         return next();
 });
 
+EventBus.onSeries('Thread.NewMessage', function(req, next) {
+    if (req.messageType === "sendMessage") {
+        async.parallel({
+            project: function(cb) {
+                Project.findById(req.project, cb);
+            },
+            team: function (cb) {
+                Team.findOne({$or:[{leader: req.message.user._id}, {member: req.message.user._id}]}, cb);
+            }
+        }, function(err, result) {
+            if (err) {return next();}
+            var newestMessage = _.last(req.messages);
+            if (req.notMembers && req.notMembers.length > 0) {
+                async.each(req.notMembers, function(member, cb) {
+                    User.findOne({email: member}, function(err, user) {
+                        if (err) 
+                            cb();
+                        else if (!user) {
+                            PackageInvite.findOne({project: req.project, to: member}, function(err, packageInvite) {
+                                if (err || !packageInvite) 
+                                    cb();
+                                else {
+                                    Mailer.sendMail('message-to-non-user.html', req.message.user.name + "<" + req._id+"@mg.buiilt.com.au" + ">", member, {
+                                        newestMessage: newestMessage,
+                                        user: member,
+                                        sendBy: req.message.user.toJSON(),
+                                        team: result.team.toJSON(),
+                                        request: req.toJSON(),
+                                        link: config.baseUrl + 'signup-invite?packageInviteToken=' + packageInvite._id,
+                                        subject: 'New message on ' + req.name
+                                    },function(err){
+                                        console.log(member);
+                                        console.log(err);
+                                        cb();
+                                    });
+                                }
+                            });
+                        } else
+                            cb();
+                    });
+                }, function() {
+                    return next();
+                });
+            } else 
+                return next();
+        });
+    } else 
+        return next();
+});
+
 EventBus.onSeries('Thread.Updated', function(req, next){
     if (req.messageType === "sendMessage") {
         async.parallel({
@@ -73,6 +123,38 @@ EventBus.onSeries('Thread.Updated', function(req, next){
                                 return cb();
                             });
                         }
+                    });
+                }, function() {
+                    return next();
+                });
+            } else if (req.notMembers && req.notMembers.length > 0) {
+                console.log("IT GOEST HERE");
+                async.each(req.notMembers, function(member, cb) {
+                    User.findOne({email: member}, function(err, user) {
+                        if (err) 
+                            cb();
+                        else if (!user) {
+                            PackageInvite.findOne({project: req.project, to: member}, function(err, packageInvite) {
+                                if (err || !packageInvite) 
+                                    cb();
+                                else {
+                                    Mailer.sendMail('message-to-non-user.html', req.editUser.name + "<" + req._id+"@mg.buiilt.com.au" + ">", member, {
+                                        newestMessage: newestMessage,
+                                        user: member,
+                                        sendBy: req.editUser.toJSON(),
+                                        team: result.team.toJSON(),
+                                        request: req.toJSON(),
+                                        link: config.baseUrl + 'signup-invite?packageInviteToken=' + packageInvite._id,
+                                        subject: 'New message on ' + req.name
+                                    },function(err){
+                                        console.log(member);
+                                        console.log(err);
+                                        cb();
+                                    });
+                                }
+                            });
+                        } else
+                            cb();
                     });
                 }, function() {
                     return next();
