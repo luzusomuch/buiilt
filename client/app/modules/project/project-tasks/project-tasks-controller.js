7b05197e362e7216da9571f696db0484246bf2a5
Vinh Nguyen <vinhnq@songphi.com>
2016-01-30 14:01:43 +0700

    [+] filter for tasks

--- client/app/modules/project/project-tasks/project-tasks-controller.js
+++ client/app/modules/project/project-tasks/project-tasks-controller.js
@@ -4,9 +4,101 @@ angular.module('buiiltApp').controller('projectTasksCtrl', function($rootScope, 
 		_.remove(task.members, {_id: $rootScope.currentUser._id});
 	});
 	$scope.tasks = tasks;
-	$scope.search = false;
-	$scope.results = [];
 	$scope.people = people;
+
+    // filter section
+    $scope.dueDate = [{text: "today", value: "today"}, {text: "tomorrow", value: "tomorrow"}, {text: "this week", value: "thisWeek"}, {text: "next week", value: "nextWeek"}];
+    $scope.assignStatus = [{text: "to me", value: "toMe"}, {text: "byMe", value: "byMe"}];
+    $scope.selectDueDate = function(dateEnd) {
+        $scope.dateEnd = dateEnd;
+        $scope.dueDateFilter = null;
+    };
+
+    $scope.selectFilterTag = function(index, type) {
+        _.each($scope.dueDate, function(date) {
+            date.select = false;
+        });
+        _.each($scope.assignStatus, function(status) {
+            status.select = false;
+        });
+        if (type === "status") {
+            $scope.assignStatus[index].select = !$scope.assignStatus[index].select;
+            $scope.status = $scope.assignStatus[index].value;
+        } else {
+            $scope.dueDate[index].select = !$scope.dueDate[index].select;
+            $scope.dueDateFilter = $scope.dueDate[index].value;
+        }
+    };
+
+    $scope.search = function(task) {
+        var found = false
+        var taskDueDate = moment(task.dateEnd).format("YYYY-MM-DD");
+        if ($scope.description && $scope.description.length > 0) {
+            if (task.description.toLowerCase().indexOf($scope.description) > -1) {
+                found = true;
+            }
+            return found;
+        } else if ($scope.dateEnd) {
+            if (moment(moment($scope.dateEnd).format("YYYY-MM-DD")).isSame(taskDueDate)) {
+                found = true;
+            }
+            return found;
+        } else if ($scope.status && $scope.status.length > 0) {
+            console.log($scope.status);
+            if ($scope.status === "toMe") {
+                console.log("[DEBUG TO ME]");
+                found = (_.findIndex(task.members, function(member) {
+                    console.log(member._id, $rootScope.currentUser._id);
+                    return member._id.toString()===$rootScope.currentUser._id.toString();
+                }) !== -1) ? true : false;
+            } else if ($scope.status === "byMe") {
+                console.log("[DEBUG BY ME]");
+                if (task.owner._id.toString()===$rootScope.currentUser._id.toString()) {
+                    found = true
+                }
+            }
+            return found;
+        } else if ($scope.dueDateFilter && $scope.dueDateFilter.length > 0) {
+            switch ($scope.dueDateFilter) {
+                case "today":
+                    var today = moment(new Date()).format("YYYY-MM-DD");
+                    if (moment(taskDueDate).isSame(today)) {
+                        found = true;
+                    }
+                break;
+
+                case "tomorrow":
+                    var tomorrow = moment(new Date()).add(1, "days").format("YYYY-MM-DD");
+                    if (moment(taskDueDate).isSame(tomorrow)) {
+                        found = true;
+                    }
+                break;
+
+                case "thisWeek":
+                    var thisWeekStartDate = moment().startOf('week').format("YYYY-MM-DD");
+                    var thisWeekEndDate = moment().endOf('week').format("YYYY-MM-DD");
+                    if (moment(taskDueDate).isSameOrAfter(thisWeekStartDate) && moment(taskDueDate).isSameOrBefore(thisWeekEndDate)) {
+                        found = true;
+                    }
+                break;
+
+                case "nextWeek":
+                    var nextWeekStartDate = moment().startOf("week").add(7, "days").format("YYYY-MM-DD");
+                    var nextWeekEndDay = moment().endOf("week").add(7, "days").format("YYYY-MM-DD");
+                    if (moment(taskDueDate).isSameOrAfter(nextWeekStartDate) && moment(taskDueDate).isSameOrBefore(nextWeekEndDay)) {
+                        found = true;
+                    }
+                break;
+
+                default:
+                break;
+            }
+            return found;
+        } else
+            return true;
+    };
+    // end filter section
+
 	$scope.showNewTaskModal = function($event) {
 		$mdDialog.show({
 		  	targetEvent: $event,
