a019ba00725cd6b20b8e706240c29a46f650ec1d
Vinh Nguyen <vinhnq@songphi.com>
2016-03-04 15:35:26 +0700

    [+] back button to project sub-module

---
+++ client/app/directives/back-button/back-button-directive.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('buiiltApp')
+.directive('backButton', function(){
+    return {
+        restrict: 'A',
+
+        link: function(scope, element, attrs) {
+            element.bind('click', goBack);
+
+            function goBack() {
+                history.back();
+                scope.$apply();
+            }
+        }
+    }
+});
