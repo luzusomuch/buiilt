3f399f6874ccba435bb3205f3819f1849de007ae
Vinh Nguyen <vinhnq@songphi.com>
2016-03-09 10:38:34 +0700

    [-] fix the notification digest

--- server/components/helpers/NotificationDigest.js
+++ server/components/helpers/NotificationDigest.js
@@ -11,16 +11,15 @@ var _ = require('lodash');
 var moment = require("moment");
 var CronJob = require('cron').CronJob;
 
-var job1 = new CronJob('00 */24 07-18 * * 1-5', function(){
-    console.log("Cron is runing");
+var job1 = new CronJob('00 00 07-18 * * 1-5', function(){
     getUserNotification();
     getNotificationNonUser();
 }, null, false, 'Australia/Melbourne');
 
-// job1.start();
-console.log("IN THE NOTIFICATION Digest");
+job1.start();
+// console.log("IN THE NOTIFICATION Digest");
 
-// var job2 = new CronJob('* * * * * *', function(){
+// var job2 = new CronJob('00 38 07-18 * * 1-5', function(){
 //     console.log("RUN EVERY 1 secnd");
 // }, null, false, 'Australia/Melbourne');
 
