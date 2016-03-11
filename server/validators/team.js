5533bbdbd60c558722660f68bacc88c9e1b9863a
Vinh Nguyen <vinhnq@songphi.com>
2016-03-11 17:06:59 +0700

    [*] change in create team for mobile version

--- server/validators/team.js
+++ server/validators/team.js
@@ -9,12 +9,15 @@ var _ = require('lodash');
 exports.validateCreate = function (req, cb) {
     req.checkBody('name', 'Team name is required').notEmpty();
     req.checkBody('type', 'Team type is required').notEmpty();
-    req.checkBody('detail.companyPhoneNumber', 'Company phone number is required').notEmpty();
-    req.checkBody('detail.companyAddress.address', 'Company address is required').notEmpty();
-    req.checkBody('detail.companyAddress.suburb', 'Company suburb is required').notEmpty();
-    req.checkBody('detail.companyAddress.postCode', 'Company postcode is required').notEmpty();
-
-    return cb(req.validationErrors(), _.pick(req.body, 'name', 'type', 'emails', 'detail'));
+    if (!req.body.isMobile) {
+        req.checkBody('detail.companyPhoneNumber', 'Company phone number is required').notEmpty();
+        req.checkBody('detail.companyAddress.address', 'Company address is required').notEmpty();
+        req.checkBody('detail.companyAddress.suburb', 'Company suburb is required').notEmpty();
+        req.checkBody('detail.companyAddress.postCode', 'Company postcode is required').notEmpty();
+        return cb(req.validationErrors(), _.pick(req.body, 'name', 'type', 'emails', 'detail'));
+    } else {
+        return cb(req.validationErrors(), _.pick(req.body, 'name', 'type', "emails"));
+    }
 };
 
 exports.validateUpdate = function (req, cb) {
