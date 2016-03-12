44470c3b6fcd7755335c158da48005ae163e5e02
Vinh Nguyen <vinhnq@songphi.com>
2016-03-12 15:09:23 +0700

    [*] change api for create device token and remove device token

--- server/api/device/device.controller.js
+++ server/api/device/device.controller.js
@@ -6,23 +6,31 @@ var _ = require('lodash');
 var async = require('async');
 
 exports.insertDevice = function(req, res) {
-    var device = new Device({
-        user: req.user._id,
-        deviceToken: req.body.deviceToken,
-        platform: req.body.deviceplatform
-    });
-    device.save(function(err){
+    Device.findOne({user: req.user._id, platform: req.body.deviceplatform}, function(err, device) {
         if (err) {return res.send(500,err);}
+        else if (!device) {
+            var newDevice = new Device({
+                user: req.user._id,
+                deviceToken: req.body.deviceToken,
+                platform: req.body.deviceplatform
+            });
+            newDevice.save(function(err){
+                if (err) {return res.send(500,err);}
+                return res.send(200);
+            });
+        } else {
+            return res.send(200);
+        }
     })
 };
 
 exports.removeDevice = function(req, res) {
-    Device.findOne({user: req.params.id}, function(err, device){
+    Device.findOne({user: req.params.id, platform: req.query.deviceplatform}, function(err, device){
         if (err) {return res.send(500,err);}
-        if (!device) {return res.send(404,err);}
+        if (!device) {return res.send(404,{message: "Not Found"});}
         device.remove(function(err, data){
             if (err) {return res.send(500,err);}
-            return res.send(200,'success');
+            return res.send(200);
         });
     });
 };
