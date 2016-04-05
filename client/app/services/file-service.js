607b68616a165e96710901a6e37f620614afdcef
Vinh Nguyen <vinhnq@songphi.com>
2016-04-05 13:56:35 +0700

    [-] remove not needed things [*] change file api

--- client/app/services/file-service.js
+++ client/app/services/file-service.js
@@ -1,122 +1,42 @@
 angular.module('buiiltApp').factory('fileService', function($resource) {
-return $resource('/api/files/:id/:type/:action', {
-    id: '@_id'},{
-    get: {
-        method: 'GET',
-    },
-    getProjectFiles: {
-        method: "GET",
-        params: {
-            action: "project-files"
+    return $resource('/api/files/:id/:type/:action', {id: '@_id'}, {
+        get: {
+            method: 'GET',
         },
-        isArray: true
-    },
-    update: {
-        method: "PUT"
-    },
-    acknowledgement: {
-        method: "GET",
-        params: {
-            action: "acknowledgement"
+        getProjectFiles: {
+            method: "GET",
+            params: {
+                action: "project-files"
+            },
+            isArray: true
+        },
+        update: {
+            method: "PUT"
+        },
+        acknowledgement: {
+            method: "GET",
+            params: {
+                action: "acknowledgement"
+            }
+        },
+        assignMoreMembers: {
+            method: "PUT", 
+            params: {
+                action: "assign-more-members"
+            }
+        },
+        lastAccess: {
+            method: "GET",
+            params: {
+                action: "last-access"
+            }
+        },
+        myFiles: {
+            method: 'GET',
+            params: {
+                action: 'my-files'
+            },
+            isArray: true
         }
-    },
-    assignMoreMembers: {
-        method: "PUT", 
-        params: {
-            action: "assign-more-members"
-        }
-    },
-    lastAccess: {
-        method: "GET",
-        params: {
-            action: "last-access"
-        }
-    },
-    interested: {
-      method: 'PUT',
-      params: {
-        id: 'id',
-        action: 'interested'
-      }
-    },
-    disinterested: {
-      method: 'PUT',
-      params: {
-        id: 'id',
-        action: 'disinterested'
-      }
-    },
-    getFileByStateParam: {
-      method: 'GET',
-      params: {
-        id: 'id',
-        action: 'params'
-      },
-      isArray: true
-    },
-    downloadFile: {
-      method: 'GET',
-      params: {
-        id: 'id',
-        action: 'download'
-      }
-    },
-    downloadAll: {
-      method: 'GET',
-      params: {
-        id: 'id',
-        action: 'download-all'
-      },
-      isArray: true
-    },
-    getAll: {
-      method: 'get',
-      params: {
-        action: 'get-all'
-      },
-      isArray: true
-    },
-    getFileByPackage: {
-      method: 'get',
-      params: {
-        action: 'get-by-package'
-      },
-      isArray: true
-    },
-    sendToDocumentation: {
-      method: 'post',
-      params: {
-        id: '@id',
-        action: 'send-to-document'
-      }
-    },
-    getFileInPeople: {
-      method: 'get',
-      params: {
-        action: 'get-in-people'
-      },
-      isArray: true
-    },
-    getFileInBoard: {
-      method: 'get',
-      params: {
-        action: 'get-in-board'
-      },
-      isArray: true
-    },
-    getFileInProject: {
-      method: 'get',
-      params: {
-        action: 'get-in-project'
-      },
-      isArray: true
-    },
-    myFiles: {
-      method: 'GET',
-      params: {
-        action: 'my-files'
-      },
-      isArray: true
-    }
-  });
+    });
 });
