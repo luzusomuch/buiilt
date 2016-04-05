bbefb24012613477383eb8407629c7f2fa3e87ad
Vinh Nguyen <vinhnq@songphi.com>
2016-04-05 14:36:25 +0700

    [*] change people service and it's API

--- client/app/services/people-service.js
+++ client/app/services/people-service.js
@@ -1,66 +1,20 @@
-angular.module('buiiltApp')
-.factory('peopleService', function($rootScope, $q, $resource) {
-    return $resource('/api/peoples/:id/:tenderId/:action',{id: '@_id', tenderId: '@_tenderId'},
-        {
-            update: {
-                method: 'PUT',
-                params: {
-                    action: 'invite'
-                }
-            },
-            selectWinnerTender: {
-                method: 'put',
-                params: {
-                    action: 'select-winner-tender'
-                }
-            },
-            getInvitePeople: {
-                method: 'get',
-                params: {
-                    action: 'get-invite-people'
-                }
-            },
-            getTender: {
-                method: 'GET',
-                params: {
-                    action: 'get-tender'
-                }
-            },
-            updateDistributeStatus: {
-                method: "get",
-                params: {
-                    action: "update-distribute-status"
-                }
-            },
-            attachAddendum: {
-                method: "POST",
-                params: {
-                    action: "attach-addendum"
-                }
-            },
-            updateTender: {
-                method: "PUT",
-                params: {
-                    action: "update-tender"
-                }
-            },
-            submitATender: {
-                method: "POST",
-                params: {
-                    action: "submit-a-tender"
-                }
-            },
-            createRelatedItem: {
-                method: "PUT",
-                params: {
-                    action: "create-related-item"
-                }
-            },
-            acknowledgement: {
-                method: "GET",
-                params: {
-                    action: "acknowledgement"
-                }
+angular.module('buiiltApp').factory('peopleService', function($resource) {
+    return $resource('/api/peoples/:id/:tenderId/:action',{
+        id: '@_id', 
+        tenderId: '@_tenderId'
+    },
+    {
+        update: {
+            method: 'PUT',
+            params: {
+                action: 'invite'
             }
-        });
+        },
+        getInvitePeople: {
+            method: 'get',
+            params: {
+                action: 'get-invite-people'
+            }
+        }
+    });
 });
