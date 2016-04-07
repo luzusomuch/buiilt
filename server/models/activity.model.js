615e523ec9c0db5e5566b6469512f82ee2d487ee
Vinh Nguyen <vinhnq@songphi.com>
2016-04-07 14:00:39 +0700

    [+] activity.model.js

---
+++ server/models/activity.model.js
@@ -0,0 +1,61 @@
+'use strict';
+var s3 = require('../components/S3');
+var EventBus = require('./../components/EventBus');
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var ActivitySchema = new Schema({
+    owner: {type: Schema.Types.ObjectId, ref: "User", required: true},
+    project: {type: Schema.Types.ObjectId, ref: "Project", required: true},
+    estimated: {
+        date: {
+            start: Date,
+            end: Date
+        },
+        time: {
+            start: Date,
+            end: Date
+        }
+    },
+    actual: {
+        date: {
+            start: Date,
+            end: Date
+        },
+        time: {
+            start: Date,
+            end: Date
+        }
+    },
+    percentageComplete: Number,
+    relatedItem: [{
+        type: String,
+        _id: Schema.Types.ObjectId,
+        element: {}
+    }],
+    members: [{type: Schema.Types.ObjectId, ref: "User"}],
+    notMembers: [String],
+    createdAt: {type: Date, default: new Date()},
+    updatedAt: type: Date,
+    dependencies: [{
+        activity: {type: Schema.Types.ObjectId, ref: "Activity"},
+        lag: Number
+    }],
+});
+
+ActivitySchema.pre('save', function(next) {
+    this.wasNew = this.isNew;
+    this.editUser = this._editUser;
+    if (!this.isNew){
+        this.updatedAt = new Date();
+    }
+    next();
+});
+
+ActivitySchema.post('save', function(doc){
+    var evtName = this.wasNew ? 'Activity.Inserted' : 'Activity.Updated';
+    doc.editUser = this._editUser;
+    EventBus.emit(evtName, doc);
+});
+
+module.exports = mongoose.model('Activity', ActivitySchema);
