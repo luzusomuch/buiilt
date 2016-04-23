21b9c48447635568e2cc9840bec5fd59e9874118
Vinh Nguyen <vinhnq@songphi.com>
2016-04-23 15:17:02 +0700

    [-] remove sign up with package invite token

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -17,7 +17,6 @@ router.get('/:id', auth.isAuthenticated(), controller.show);
 router.get('/:id/reset-password', controller.getResetPasswordToken);
 
 router.post('/', controller.create);
-router.post('/invite-token', controller.createUserWithInviteToken);
 router.post('/send-verification', auth.isAuthenticated(), controller.sendVerification);
 router.post('/forgot-password', controller.forgotPassword);
 router.post('/reset-password', controller.resetPassword);
