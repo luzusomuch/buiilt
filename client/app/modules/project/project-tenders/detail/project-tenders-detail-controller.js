87c11414d5c95e50852f9655518ee5f17c952ed2
Myles Condon <myles@buiilt.com>
2016-04-27 10:47:09 +1000

    init project tenders

---
+++ client/app/modules/project/project-tenders/detail/project-tenders-detail-controller.js
@@ -0,0 +1,3 @@
+angular.module('buiiltApp').controller('projectTendersDetailCtrl', function($q, $rootScope, $scope, $timeout, $stateParams, messageService, $mdToast, $mdDialog, $state, thread, peopleService, taskService, uploadService, people, clipboard, socket, notificationService) {
+    
+});
---
+++ client/app/modules/project/project-tenders/project-tenders-controller.js
@@ -0,0 +1,11 @@
+angular.module('buiiltApp').controller('projectTendersCtrl', function($rootScope, $scope, $timeout, $mdDialog, peopleService, $stateParams, $state, $mdToast, messageService, people, socket, notificationService, dialogService, activities) {
+	$rootScope.title = $rootScope.project.name +" Tenders";
+    // $scope.people = people;
+// 	$scope.threads = threads;
+//     $scope.activities = activities;
+//     $scope.dialogService = dialogService;
+//     $scope.selectedFilterEventList = [];
+//     $scope.selectedFilterRecepientList = [];
+
+    
+});
